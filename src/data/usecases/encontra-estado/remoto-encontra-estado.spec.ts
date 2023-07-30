0319146f5b60263b124dc9cf16a1e124f71f996c
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-30 17:03:49 -0300

    refactor: move criacao de SUT para metodo factory

--- src/data/usecases/encontra-estado/remoto-encontra-estado.spec.ts
+++ src/data/usecases/encontra-estado/remoto-encontra-estado.spec.ts
@@ -2,11 +2,24 @@ import { ClienteHttpSpy } from '../../test';
 import { mockEntradaEncontraEstadoDTO } from '../../test/mock-estado';
 import { RemotoEncontraEstado } from './remoto-encontra-estado';
 
+type SutTypes = {
+  sut: RemotoEncontraEstado
+  clienteHttpSpy: ClienteHttpSpy
+};
+
+const criaSut = (url: string = 'qualquer_url'): SutTypes => {
+  const clienteHttpSpy = new ClienteHttpSpy();
+  const sut = new RemotoEncontraEstado(url, clienteHttpSpy);
+  return {
+    sut,
+    clienteHttpSpy
+  };
+};
+
 describe('RemotoEncontraEstado', () => {
   test('deve chamar ClienteHttp com os valores corretos', async () => {
     const url = 'outra_url';
-    const clienteHttpSpy = new ClienteHttpSpy();
-    const sut = new RemotoEncontraEstado(url, clienteHttpSpy);
+    const { sut, clienteHttpSpy } = criaSut(url);
 
     await sut.encontra(mockEntradaEncontraEstadoDTO);
 
