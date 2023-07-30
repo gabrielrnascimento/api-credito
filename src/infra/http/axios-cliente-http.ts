7beed7b012c1184b3b3417e293579678a4f16a9a
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-30 18:26:13 -0300

    feat: garante que AxiosClienteHttp retorne erro correto

--- src/infra/http/axios-cliente-http.ts
+++ src/infra/http/axios-cliente-http.ts
@@ -4,7 +4,13 @@ import { type RequisicaoHttp, type RespostaHttp } from '../../data/types/http';
 
 export class AxiosClienteHttp implements ClienteHttp {
   async requisicao (requisicao: RequisicaoHttp): Promise<RespostaHttp<any>> {
-    const respostaAxios = await axios.request(requisicao);
+    let respostaAxios: any;
+    try {
+      respostaAxios = await axios.request(requisicao);
+    } catch (error) {
+      const { status, statusText } = error.response;
+      respostaAxios = { status, data: { mensagem: statusText, stack: error.stack } };
+    }
     return {
       codigoStatus: respostaAxios.status,
       body: respostaAxios.data
