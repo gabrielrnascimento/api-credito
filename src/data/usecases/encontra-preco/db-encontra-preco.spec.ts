e2793f0de8a6c4d0a094662b1836a38df8896eb6
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-07-31 15:07:17 -0300

    refactor: move criacao de SUT para metodo factory

--- src/data/usecases/encontra-preco/db-encontra-preco.spec.ts
+++ src/data/usecases/encontra-preco/db-encontra-preco.spec.ts
@@ -1,10 +1,24 @@
 import { DbEncontraPrecoRepositorioStub, mockModeloEstado } from '../../test';
 import { DbEncontraPreco } from './db-encontra-preco';
 
+type SutTypes = {
+  sut: DbEncontraPreco
+  dbEncontraPrecoRepositorio: DbEncontraPrecoRepositorioStub
+};
+
+const criaSut = (): SutTypes => {
+  const dbEncontraPrecoRepositorio = new DbEncontraPrecoRepositorioStub();
+  const sut = new DbEncontraPreco(dbEncontraPrecoRepositorio);
+
+  return {
+    sut,
+    dbEncontraPrecoRepositorio
+  };
+};
+
 describe('DbEncontraPreco', () => {
   test('deve chamar DbEncontraPrecoRepositorio com os valores corretos', async () => {
-    const dbEncontraPrecoRepositorio = new DbEncontraPrecoRepositorioStub();
-    const sut = new DbEncontraPreco(dbEncontraPrecoRepositorio);
+    const { sut, dbEncontraPrecoRepositorio } = criaSut();
     const encontraSpy = jest.spyOn(dbEncontraPrecoRepositorio, 'encontra');
 
     await sut.encontra(mockModeloEstado);
