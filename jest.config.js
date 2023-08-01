4af438ffdb1a7470ec5648a86b850a4d7e1f4f49
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-01 11:35:21 -0300

    chore: atualiza jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
     roots: ['<rootDir>/src/'],
     collectCoverageFrom: [
       '<rootDir>/src/**/*.ts',
+      '!<rootDir>/src/**/index.ts',
+      '!<rootDir>/src/**/erro-*.ts',
     ],
     preset: '@shelf/jest-mongodb',
     coverageDirectory: 'coverage',
