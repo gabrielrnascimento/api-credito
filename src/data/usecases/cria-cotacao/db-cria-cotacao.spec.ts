dd2e111d026949fe44db7467b6edd3c130eaad8a
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-01 19:58:42 -0300

    test: garante que DbCriaCotacao lance erro caso DbCriaCotacaoRepositorio lance erro

--- src/data/usecases/cria-cotacao/db-cria-cotacao.spec.ts
+++ src/data/usecases/cria-cotacao/db-cria-cotacao.spec.ts
@@ -17,11 +17,19 @@ const criaSut = (): SutTypes => {
 };
 
 describe('DbCriaCotacao', () => {
-  test('deve chamar DbCriaCotacaoRepositorioStub com os valores corretos', async () => {
+  test('deve chamar DbCriaCotacaoRepositorio com os valores corretos', async () => {
     const { sut, dbCriaCotacaoRepositorioStub } = criaSut();
     const criaSpy = jest.spyOn(dbCriaCotacaoRepositorioStub, 'cria');
     await sut.cria(mockEntradaCriaCotacaoDTO);
 
     expect(criaSpy).toHaveBeenCalledWith(mockEntradaCriaCotacaoDTO);
+  });
+
+  test('deve lançar error caso DbCriaCotacaoRepositorio lance erro', async () => {
+    const { sut, dbCriaCotacaoRepositorioStub } = criaSut();
+    jest.spyOn(dbCriaCotacaoRepositorioStub, 'cria').mockRejectedValueOnce(new Error());
+    const promessa = sut.cria(mockEntradaCriaCotacaoDTO);
+
+    await expect(promessa).rejects.toThrow(new Error());
   });
 });
