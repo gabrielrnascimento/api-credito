6025a74e0473122a4760033672d21727a35e9155
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-01 19:55:11 -0300

    refactor: move criacao de SUT para metodo factory

--- src/data/usecases/cria-cotacao/db-cria-cotacao.spec.ts
+++ src/data/usecases/cria-cotacao/db-cria-cotacao.spec.ts
@@ -1,12 +1,25 @@
 import { DbCriaCotacaoRepositorioStub, mockEntradaCriaCotacaoDTO } from '../../test/mock-cotacao';
 import { DbCriaCotacao } from './db-cria-cotacao';
 
+type SutTypes = {
+  sut: DbCriaCotacao
+  dbCriaCotacaoRepositorioStub: DbCriaCotacaoRepositorioStub
+};
+
+const criaSut = (): SutTypes => {
+  const dbCriaCotacaoRepositorioStub = new DbCriaCotacaoRepositorioStub();
+  const sut = new DbCriaCotacao(dbCriaCotacaoRepositorioStub);
+
+  return {
+    sut,
+    dbCriaCotacaoRepositorioStub
+  };
+};
+
 describe('DbCriaCotacao', () => {
   test('deve chamar DbCriaCotacaoRepositorioStub com os valores corretos', async () => {
-    const dbCriaCotacaoRepositorioStub = new DbCriaCotacaoRepositorioStub();
+    const { sut, dbCriaCotacaoRepositorioStub } = criaSut();
     const criaSpy = jest.spyOn(dbCriaCotacaoRepositorioStub, 'cria');
-    const sut = new DbCriaCotacao(dbCriaCotacaoRepositorioStub);
-
     await sut.cria(mockEntradaCriaCotacaoDTO);
 
     expect(criaSpy).toHaveBeenCalledWith(mockEntradaCriaCotacaoDTO);
