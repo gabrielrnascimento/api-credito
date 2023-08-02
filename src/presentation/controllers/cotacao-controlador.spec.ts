9bc13ba06c85f97d274907425c221c48c66245d7
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-02 14:13:13 -0300

    refactor: move criacao de SUT para metodo factory

--- src/presentation/controllers/cotacao-controlador.spec.ts
+++ src/presentation/controllers/cotacao-controlador.spec.ts
@@ -1,11 +1,24 @@
 import { EncontraEstadoStub, mockRequisicaoHttpCriaCotacao } from '../test';
 import { CotacaoControlador } from './cotacao-controlador';
 
+type SutTypes = {
+  sut: CotacaoControlador
+  encontraEstadoStub: EncontraEstadoStub
+};
+
+const criaSut = (): SutTypes => {
+  const encontraEstadoStub = new EncontraEstadoStub();
+  const sut = new CotacaoControlador(encontraEstadoStub);
+  return {
+    sut,
+    encontraEstadoStub
+  };
+};
+
 describe('CotacaoControlador', () => {
   test('deve chamar EncontraEstado com os valores corretos', async () => {
-    const encontraEstadoStub = new EncontraEstadoStub();
+    const { sut, encontraEstadoStub } = criaSut();
     const encontraEstadoSpy = jest.spyOn(encontraEstadoStub, 'encontraEstado');
-    const sut = new CotacaoControlador(encontraEstadoStub);
 
     await sut.trate(mockRequisicaoHttpCriaCotacao);
 
