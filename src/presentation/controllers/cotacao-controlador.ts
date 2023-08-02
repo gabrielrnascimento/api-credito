e1a29575fdbdacfed1dd6430a5fc9f8cdba8e38d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-02 15:07:01 -0300

    feat: garante que CotacaoControlador chame CalculaCredito com os valores corretos

--- src/presentation/controllers/cotacao-controlador.ts
+++ src/presentation/controllers/cotacao-controlador.ts
@@ -1,6 +1,6 @@
 import { type EntradaControladorCriaCotacaoDTO } from '../../data/dtos';
 import { ErroEstadoNaoEncontrado } from '../../domain/errors';
-import { type EncontraPreco, type EncontraEstado } from '../../domain/usecases';
+import { type EncontraPreco, type EncontraEstado, type CalculaCredito } from '../../domain/usecases';
 import { ErroNaoEncontrado, ErroRequisicaoInvalida } from '../errors';
 import { type Controlador, type RequisicaoHttp, type RespostaHttp } from '../interfaces';
 import { erroServidor, naoEncontrado, requisicaoInvalida } from '../utils';
@@ -8,17 +8,23 @@ import { erroServidor, naoEncontrado, requisicaoInvalida } from '../utils';
 export class CotacaoControlador implements Controlador {
   constructor (
     private readonly encontraEstado: EncontraEstado,
-    private readonly encontraPreco: EncontraPreco
+    private readonly encontraPreco: EncontraPreco,
+    private readonly calculaCredito: CalculaCredito
   ) {
     this.encontraEstado = encontraEstado;
     this.encontraPreco = encontraPreco;
   }
 
   async trate (requisicao: RequisicaoHttp<EntradaControladorCriaCotacaoDTO>): Promise<RespostaHttp> {
-    const { cep } = requisicao.body;
+    const { cep, dataPagamento, quantidade } = requisicao.body;
     try {
       const { uf } = await this.encontraEstado.encontraEstado({ cep });
-      await this.encontraPreco.encontraPreco({ uf });
+      const { preco } = await this.encontraPreco.encontraPreco({ uf });
+      this.calculaCredito.calcula({
+        valorUnitario: preco,
+        quantidade,
+        dataPagamento
+      });
     } catch (erro) {
       switch (true) {
         case erro instanceof ErroRequisicaoInvalida: return requisicaoInvalida(erro);
