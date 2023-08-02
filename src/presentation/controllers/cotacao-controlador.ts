74dc3802dffd1832a71a37c9db100978f0cf323a
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-02 15:13:56 -0300

    feat: garante que CotacaoControlador chame CriaCotacao com os valores corretos

--- src/presentation/controllers/cotacao-controlador.ts
+++ src/presentation/controllers/cotacao-controlador.ts
@@ -1,6 +1,6 @@
 import { type EntradaControladorCriaCotacaoDTO } from '../../data/dtos';
 import { ErroEstadoNaoEncontrado } from '../../domain/errors';
-import { type EncontraPreco, type EncontraEstado, type CalculaCredito } from '../../domain/usecases';
+import { type EncontraPreco, type EncontraEstado, type CalculaCredito, type CriaCotacao } from '../../domain/usecases';
 import { ErroNaoEncontrado, ErroRequisicaoInvalida } from '../errors';
 import { type Controlador, type RequisicaoHttp, type RespostaHttp } from '../interfaces';
 import { erroServidor, naoEncontrado, requisicaoInvalida } from '../utils';
@@ -9,21 +9,29 @@ export class CotacaoControlador implements Controlador {
   constructor (
     private readonly encontraEstado: EncontraEstado,
     private readonly encontraPreco: EncontraPreco,
-    private readonly calculaCredito: CalculaCredito
+    private readonly calculaCredito: CalculaCredito,
+    private readonly criaCotacao: CriaCotacao
   ) {
     this.encontraEstado = encontraEstado;
     this.encontraPreco = encontraPreco;
   }
 
   async trate (requisicao: RequisicaoHttp<EntradaControladorCriaCotacaoDTO>): Promise<RespostaHttp> {
-    const { cep, dataPagamento, quantidade } = requisicao.body;
+    const { nome, cep, dataPagamento, quantidade } = requisicao.body;
     try {
       const { uf } = await this.encontraEstado.encontraEstado({ cep });
       const { preco } = await this.encontraPreco.encontraPreco({ uf });
-      this.calculaCredito.calcula({
+      const valor = this.calculaCredito.calcula({
         valorUnitario: preco,
         quantidade,
         dataPagamento
+      });
+      await this.criaCotacao.cria({
+        nome,
+        uf,
+        quantidade,
+        dataPagamento,
+        valor
       });
     } catch (erro) {
       switch (true) {
