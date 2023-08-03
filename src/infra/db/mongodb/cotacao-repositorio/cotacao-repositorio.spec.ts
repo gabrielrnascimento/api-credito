e249f2599e2277a70c39ddf60bd269929e5b78db
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-03 18:24:13 -0300

    test: garante que CotacaoRepositorio retorne lista de ModeloCotacao com as 10 últimas cotações ordenadas por data de vencimento em ordem crescente em caso de sucesso

--- src/infra/db/mongodb/cotacao-repositorio/cotacao-repositorio.spec.ts
+++ src/infra/db/mongodb/cotacao-repositorio/cotacao-repositorio.spec.ts
@@ -2,6 +2,16 @@ import { ObjectId } from 'mongodb';
 import { MongoUtil } from '../util/mongo-util';
 import { mockEntradaDbCriaCotacaoRepositorioDTO, mockModeloCotacao } from '../../../../data/test';
 import { CotacaoRepositorio } from './cotacao-repositorio';
+import { seedCotacoes } from '../util/seeds';
+
+type SutTypes = {
+  sut: CotacaoRepositorio
+};
+
+const criaSut = (db: MongoUtil): SutTypes => {
+  const sut = new CotacaoRepositorio(db);
+  return { sut };
+};
 
 describe('CotacaoRepositorio', () => {
   let db: MongoUtil;
@@ -19,23 +29,51 @@ describe('CotacaoRepositorio', () => {
     await db.limpa('cotacoes');
   });
 
-  test('deve retornar ModeloCotacao em caso de sucesso', async () => {
-    const sut = new CotacaoRepositorio(db);
+  describe('cria()', () => {
+    test('deve retornar ModeloCotacao em caso de sucesso', async () => {
+      const { sut } = criaSut(db);
 
-    const dataAtual = new Date();
+      const dataAtual = new Date();
 
-    const resposta = await sut.cria(mockEntradaDbCriaCotacaoRepositorioDTO(dataAtual));
+      const resposta = await sut.cria(mockEntradaDbCriaCotacaoRepositorioDTO(dataAtual));
 
-    const { nome, estado, quantidade, valor, dataVencimento } = mockModeloCotacao;
+      const { nome, estado, quantidade, valor, dataVencimento } = mockModeloCotacao;
 
-    expect(resposta).toEqual({
-      id: expect.any(ObjectId),
-      nome,
-      estado,
-      quantidade,
-      valor,
-      dataCriacao: dataAtual,
-      dataVencimento
+      expect(resposta).toEqual({
+        id: expect.any(ObjectId),
+        nome,
+        estado,
+        quantidade,
+        valor,
+        dataCriacao: dataAtual,
+        dataVencimento
+      });
+    });
+  });
+
+  describe('lista()', () => {
+    test('deve retornar lista de ModeloCotacao com as 10 últimas cotações ordenadas por data de vencimento em ordem crescente em caso de sucesso', async () => {
+      await db.popula('cotacoes', seedCotacoes);
+      const mockCotacoes = seedCotacoes
+        .sort((a: any, b: any) => b.dataCriacao - a.dataCriacao)
+        .slice(0, 10)
+        .sort((a: any, b: any) => a.dataVencimento - b.dataVencimento);
+      const { sut } = criaSut(db);
+
+      const resposta = await sut.lista();
+
+      expect(resposta).toHaveLength(10);
+      resposta.forEach((cotacao, index) => {
+        expect(cotacao).toEqual({
+          id: expect.any(ObjectId),
+          nome: mockCotacoes[index].nome,
+          estado: mockCotacoes[index].estado,
+          quantidade: mockCotacoes[index].quantidade,
+          valor: mockCotacoes[index].valor,
+          dataCriacao: mockCotacoes[index].dataCriacao,
+          dataVencimento: mockCotacoes[index].dataVencimento
+        });
+      });
     });
   });
 });
---
+++ src/infra/db/mongodb/util/seeds/cotacoes.ts
@@ -0,0 +1,162 @@
+export const seedCotacoes = [
+  {
+    nome: 'Exemplo 1',
+    estado: 'SP',
+    quantidade: 5,
+    valor: 5000.0,
+    dataCriacao: new Date('2023-01-15'),
+    dataVencimento: new Date('2023-07-15')
+  },
+  {
+    nome: 'Exemplo 2',
+    estado: 'MG',
+    quantidade: 8,
+    valor: 7321.75,
+    dataCriacao: new Date('2023-02-18'),
+    dataVencimento: new Date('2023-08-18')
+  },
+  {
+    nome: 'Exemplo 3',
+    estado: 'ES',
+    quantidade: 12,
+    valor: 10456.3,
+    dataCriacao: new Date('2023-03-05'),
+    dataVencimento: new Date('2023-09-05')
+  },
+  {
+    nome: 'Exemplo 4',
+    estado: 'BA',
+    quantidade: 3,
+    valor: 2198.9,
+    dataCriacao: new Date('2023-04-22'),
+    dataVencimento: new Date('2023-10-22')
+  },
+  {
+    nome: 'Exemplo 5',
+    estado: 'SP',
+    quantidade: 15,
+    valor: 15789.6,
+    dataCriacao: new Date('2023-05-10'),
+    dataVencimento: new Date('2023-11-10')
+  },
+  {
+    nome: 'Exemplo 6',
+    estado: 'MG',
+    quantidade: 9,
+    valor: 8234.1,
+    dataCriacao: new Date('2023-06-28'),
+    dataVencimento: new Date('2023-12-28')
+  },
+  {
+    nome: 'Exemplo 7',
+    estado: 'ES',
+    quantidade: 6,
+    valor: 6123.45,
+    dataCriacao: new Date('2023-07-03'),
+    dataVencimento: new Date('2024-01-03')
+  },
+  {
+    nome: 'Exemplo 8',
+    estado: 'BA',
+    quantidade: 10,
+    valor: 9321.8,
+    dataCriacao: new Date('2023-08-11'),
+    dataVencimento: new Date('2024-02-11')
+  },
+  {
+    nome: 'Exemplo 9',
+    estado: 'SP',
+    quantidade: 7,
+    valor: 7210.2,
+    dataCriacao: new Date('2023-09-17'),
+    dataVencimento: new Date('2024-03-17')
+  },
+  {
+    nome: 'Exemplo 10',
+    estado: 'MG',
+    quantidade: 11,
+    valor: 10987.6,
+    dataCriacao: new Date('2023-10-29'),
+    dataVencimento: new Date('2024-04-29')
+  },
+  {
+    nome: 'Exemplo 11',
+    estado: 'SP',
+    quantidade: 4,
+    valor: 4123.9,
+    dataCriacao: new Date('2023-11-12'),
+    dataVencimento: new Date('2024-05-12')
+  },
+  {
+    nome: 'Exemplo 12',
+    estado: 'MG',
+    quantidade: 6,
+    valor: 6021.2,
+    dataCriacao: new Date('2023-12-19'),
+    dataVencimento: new Date('2024-06-19')
+  },
+  {
+    nome: 'Exemplo 13',
+    estado: 'ES',
+    quantidade: 9,
+    valor: 9432.5,
+    dataCriacao: new Date('2024-01-25'),
+    dataVencimento: new Date('2024-07-25')
+  },
+  {
+    nome: 'Exemplo 14',
+    estado: 'BA',
+    quantidade: 2,
+    valor: 1897.3,
+    dataCriacao: new Date('2024-02-06'),
+    dataVencimento: new Date('2024-08-06')
+  },
+  {
+    nome: 'Exemplo 15',
+    estado: 'SP',
+    quantidade: 7,
+    valor: 7324.8,
+    dataCriacao: new Date('2024-03-14'),
+    dataVencimento: new Date('2024-09-14')
+  },
+  {
+    nome: 'Exemplo 16',
+    estado: 'MG',
+    quantidade: 11,
+    valor: 11023.6,
+    dataCriacao: new Date('2024-04-02'),
+    dataVencimento: new Date('2024-10-02')
+  },
+  {
+    nome: 'Exemplo 17',
+    estado: 'ES',
+    quantidade: 5,
+    valor: 5321.1,
+    dataCriacao: new Date('2024-05-09'),
+    dataVencimento: new Date('2024-11-09')
+  },
+  {
+    nome: 'Exemplo 18',
+    estado: 'BA',
+    quantidade: 8,
+    valor: 8123.4,
+    dataCriacao: new Date('2024-06-18'),
+    dataVencimento: new Date('2024-12-18')
+  },
+  {
+    nome: 'Exemplo 19',
+    estado: 'SP',
+    quantidade: 3,
+    valor: 3210.5,
+    dataCriacao: new Date('2024-07-27'),
+    dataVencimento: new Date('2025-01-27')
+  },
+  {
+    nome: 'Exemplo 20',
+    estado: 'MG',
+    quantidade: 9,
+    valor: 9231.7,
+    dataCriacao: new Date('2024-06-27'),
+    dataVencimento: new Date('2025-01-03')
+  }
+];
