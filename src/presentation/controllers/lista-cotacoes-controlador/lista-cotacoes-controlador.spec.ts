d9434bcad31022eb3819591cd9ebcc597cb19a5e
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-03 18:39:09 -0300

    refactor: move criacao de SUT para metodo factory

--- src/presentation/controllers/lista-cotacoes-controlador/lista-cotacoes-controlador.spec.ts
+++ src/presentation/controllers/lista-cotacoes-controlador/lista-cotacoes-controlador.spec.ts
@@ -1,10 +1,23 @@
 import { ListaCotacoesStub } from '../../test';
 import { ListaCotacoesControlador } from './lista-cotacoes-controlador';
 
+type SutTypes = {
+  sut: ListaCotacoesControlador
+  listaCotacoesStub: ListaCotacoesStub
+};
+
+const criaSut = (): SutTypes => {
+  const listaCotacoesStub = new ListaCotacoesStub();
+  const sut = new ListaCotacoesControlador(listaCotacoesStub);
+  return {
+    sut,
+    listaCotacoesStub
+  };
+};
+
 describe('ListaCotacoesControlador', () => {
   test('deve chamar ListaCotacoes corretamente', async () => {
-    const listaCotacoesStub = new ListaCotacoesStub();
-    const sut = new ListaCotacoesControlador(listaCotacoesStub);
+    const { sut, listaCotacoesStub } = criaSut();
     const listaSpy = jest.spyOn(listaCotacoesStub, 'lista');
 
     await sut.trate();
