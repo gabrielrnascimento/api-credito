75cc3548ca9efb3235cd7bb66abd58878225d733
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-03 18:48:24 -0300

    feat: garante que ListaCotacoesControlador retorne 500 caso ListaCotacoes lance um erro

--- src/presentation/controllers/lista-cotacoes-controlador/lista-cotacoes-controlador.ts
+++ src/presentation/controllers/lista-cotacoes-controlador/lista-cotacoes-controlador.ts
@@ -1,5 +1,6 @@
 import { type ListaCotacoes } from '../../../domain/usecases';
 import { type Controlador, type RespostaHttp } from '../../interfaces';
+import { erroServidor } from '../../utils';
 
 export class ListaCotacoesControlador implements Controlador {
   constructor (private readonly listaCotacoes: ListaCotacoes) {
@@ -7,7 +8,10 @@ export class ListaCotacoesControlador implements Controlador {
   }
 
   async trate (): Promise<RespostaHttp> {
-    await this.listaCotacoes.lista();
-    return null;
+    try {
+      await this.listaCotacoes.lista();
+    } catch (erro) {
+      return erroServidor(erro);
+    }
   }
 }
