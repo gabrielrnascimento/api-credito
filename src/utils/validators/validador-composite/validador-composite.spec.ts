2c0cb5e6d2f9a17c8c56a51b66827818b7d8b586
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-02 22:14:18 -0300

    refactor: move criacao de SUT para metodo factory

--- src/utils/validators/validador-composite/validador-composite.spec.ts
+++ src/utils/validators/validador-composite/validador-composite.spec.ts
@@ -1,14 +1,29 @@
 import { ValidadorStub } from '../../../presentation/test';
 import { ValidadorComposite } from './validador-composite';
 
+type SutTypes = {
+  sut: ValidadorComposite
+  campo: string
+  validadorStubs: ValidadorStub[]
+};
+
+const criaSut = (): SutTypes => {
+  const campo = 'qualquerCampo';
+  const validadorStubs = [
+    new ValidadorStub(),
+    new ValidadorStub()
+  ];
+  const sut = new ValidadorComposite(validadorStubs);
+  return {
+    sut,
+    campo,
+    validadorStubs
+  };
+};
+
 describe('ValidadorComposite', () => {
   test('deve retornar erro caso alguma validação falhe', () => {
-    const campo = 'qualquerCampo';
-    const validadorStubs = [
-      new ValidadorStub(),
-      new ValidadorStub()
-    ];
-    const sut = new ValidadorComposite(validadorStubs);
+    const { sut, campo, validadorStubs } = criaSut();
     jest.spyOn(validadorStubs[0], 'valida').mockReturnValueOnce(new Error());
 
     const erro = sut.valida({ [campo]: 'qualquer_valor' });
