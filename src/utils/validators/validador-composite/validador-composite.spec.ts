054826ae9a2b1aa2907af5dd88afd98a7652fd05
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-02 22:14:58 -0300

    test: garante que ValidadorComposite retorne o primeiro erro caso mais de uma validação falhe

--- src/utils/validators/validador-composite/validador-composite.spec.ts
+++ src/utils/validators/validador-composite/validador-composite.spec.ts
@@ -1,4 +1,5 @@
 import { ValidadorStub } from '../../../presentation/test';
+import { ErroDataInvalida } from '../../errors';
 import { ValidadorComposite } from './validador-composite';
 
 type SutTypes = {
@@ -29,5 +30,15 @@ describe('ValidadorComposite', () => {
     const erro = sut.valida({ [campo]: 'qualquer_valor' });
 
     expect(erro).toEqual(new Error());
+  });
+
+  test('deve retornar o primeiro erro caso mais de uma validação falhe', () => {
+    const { sut, campo, validadorStubs } = criaSut();
+    jest.spyOn(validadorStubs[0], 'valida').mockReturnValueOnce(new ErroDataInvalida());
+    jest.spyOn(validadorStubs[1], 'valida').mockReturnValueOnce(new Error());
+
+    const erro = sut.valida({ [campo]: 'qualquer_valor' });
+
+    expect(erro).toEqual(new ErroDataInvalida());
   });
 });
