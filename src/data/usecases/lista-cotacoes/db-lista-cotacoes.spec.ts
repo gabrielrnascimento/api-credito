f449d85f30ae2cdc157893b9b4f96ae6a70e4126
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-03 16:35:44 -0300

    refactor: move criacao de SUT para metodo factory

--- src/data/usecases/lista-cotacoes/db-lista-cotacoes.spec.ts
+++ src/data/usecases/lista-cotacoes/db-lista-cotacoes.spec.ts
@@ -1,10 +1,23 @@
 import { DbListaCotacoesRepositorioStub } from '../../test';
 import { DbListaCotacoes } from './db-lista-cotacoes';
 
+type SutTypes = {
+  sut: DbListaCotacoes
+  dbListaCotacoesRepositorioStub: DbListaCotacoesRepositorioStub
+};
+
+const criaSut = (): SutTypes => {
+  const dbListaCotacoesRepositorioStub = new DbListaCotacoesRepositorioStub();
+  const sut = new DbListaCotacoes(dbListaCotacoesRepositorioStub);
+  return {
+    sut,
+    dbListaCotacoesRepositorioStub
+  };
+};
+
 describe('DbListaCotacoes', () => {
   test('deve chamar DbListaCotacoesRepositorio correamente', async () => {
-    const dbListaCotacoesRepositorioStub = new DbListaCotacoesRepositorioStub();
-    const sut = new DbListaCotacoes(dbListaCotacoesRepositorioStub);
+    const { sut, dbListaCotacoesRepositorioStub } = criaSut();
     const listaSpy = jest.spyOn(dbListaCotacoesRepositorioStub, 'lista');
 
     await sut.lista();
