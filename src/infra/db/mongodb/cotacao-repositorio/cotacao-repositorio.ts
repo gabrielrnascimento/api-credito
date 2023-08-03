521bc68a7e1e67dd37bb9e82d4b057da9efa2119
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-08-03 18:24:07 -0300

    feat: garante que CotacaoRepositorio retorne lista de ModeloCotacao com as 10 últimas cotações ordenadas por data de vencimento em ordem crescente em caso de sucesso

--- src/infra/db/mongodb/cotacao-repositorio/cotacao-repositorio.ts
+++ src/infra/db/mongodb/cotacao-repositorio/cotacao-repositorio.ts
@@ -1,9 +1,9 @@
 import { type EntradaDbCriaCotacaoRepositorioDTO } from '../../../../data/dtos';
-import { type DbCriaCotacaoRepositorio } from '../../../../data/interfaces';
+import { type DbListaCotacoesRepositorio, type DbCriaCotacaoRepositorio } from '../../../../data/interfaces';
 import { type ModeloCotacao } from '../../../../domain/models';
 import { type MongoUtil } from '../util/mongo-util';
 
-export class CotacaoRepositorio implements DbCriaCotacaoRepositorio {
+export class CotacaoRepositorio implements DbCriaCotacaoRepositorio, DbListaCotacoesRepositorio {
   constructor (private readonly mongoClient: MongoUtil) {
     this.mongoClient = mongoClient;
   }
@@ -12,5 +12,17 @@ export class CotacaoRepositorio implements DbCriaCotacaoRepositorio {
     const collection = await this.mongoClient.pegaCollection('cotacoes');
     await collection.insertOne(dados);
     return this.mongoClient.formata(dados);
+  }
+
+  async lista (): Promise<ModeloCotacao[]> {
+    const collection = await this.mongoClient.pegaCollection('cotacoes');
+    const cotacoesPorDataCriacaoDecrescente = await collection.find()
+      .sort({ dataCriacao: -1 })
+      .limit(10)
+      .toArray();
+
+    const cotacoesPorDataVencimentoCrescente = cotacoesPorDataCriacaoDecrescente.sort((a, b) => a.dataVencimento - b.dataVencimento);
+    const resposta = cotacoesPorDataVencimentoCrescente.map(cotacao => this.mongoClient.formata(cotacao));
+    return resposta;
   }
 }
